01336f710c20d1cfab15f5a9baac2604738ba4b6
Maximiliano Rodrigo Soria <maximilianorodrigosoria@gmail.com>
2022-02-13 09:24:21 -0300

    commit inicial

---
+++ src/ar/com/practicas/Main.java
@@ -0,0 +1,10 @@
+package ar.com.practicas;
+
+public class Main {
+
+    public static void main(String[] args) {
+	  // Este es mi primer programa
+        System.out.println("Hola Mundo 2!!!"); // sout
+
+    }
+}
