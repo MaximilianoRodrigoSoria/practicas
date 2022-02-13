9bd03cba8e8e0de4bdbc2759075444ebf52929ec
Maximiliano Rodrigo Soria <maximilianorodrigosoria@gmail.com>
2022-02-13 15:50:15 -0300

    Operadores de asignacion

---
+++ src/ar/com/practicas/OperadoresAsignacion.java
@@ -0,0 +1,30 @@
+package ar.com.practicas;
+
+public class OperadoresAsignacion {
+
+    public static void main(String[] args) {
+
+        //Asignar Valores
+        int varA = 23, varB=12;
+
+
+        int varC = varA + 5 - varB;
+        System.out.println("varC = " + varC);
+
+        //Aginacion +=   -->  //a = a + 1
+        varA += 1;
+        System.out.println("varA = " + varA);
+
+        //Aginacion +=   -->  //a = a + 3
+        varA += 3;
+        System.out.println("varA = " + varA);
+
+        //Aginacion -=   -->  //a = a - 2
+        varA -= 2;
+        System.out.println("varA = " + varA);
+
+        //Otros
+
+        // *=   /=   %=
+    }
+}
